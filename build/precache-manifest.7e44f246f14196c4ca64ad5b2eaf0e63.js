56848b92fff33fff5b1c55b0bba5694401d3f03a
Areatomic <aureliensebe@gmail.com>
2020-05-07 11:16:31 +0200

    'production'

---
+++ build/precache-manifest.7e44f246f14196c4ca64ad5b2eaf0e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb059e5cab7753a1b9e515bc022d6d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064fd4b266ab9728d1d3",
+    "url": "/static/css/main.10296410.chunk.css"
+  },
+  {
+    "revision": "be8faafd75d0a42d70e3",
+    "url": "/static/js/2.8d9e63ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d9e63ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064fd4b266ab9728d1d3",
+    "url": "/static/js/main.fa452412.chunk.js"
+  },
+  {
+    "revision": "c7f9702e0a76347033f1",
+    "url": "/static/js/runtime-main.9b8ceca5.js"
+  },
+  {
+    "revision": "40c8f969f079d0dfbfd95b8d58aabadc",
+    "url": "/static/media/undraw_medicine_b1ol.40c8f969.svg"
+  }
+]);
